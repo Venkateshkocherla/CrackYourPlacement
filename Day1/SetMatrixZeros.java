3e399aca619367351e5970162cdb871772a890cf
Venkatesh Kocherla <86240365+Venkateshkocherla@users.noreply.github.com>
2023-08-11 22:49:52 +0530

    Add files via upload

---
+++ Day1/SetMatrixZeros.java
@@ -0,0 +1,37 @@
+class SetMatrixZeros{
+    public void setZeroes(int[][] matrix) {
+        int m=matrix.length, n=matrix[0].length;
+        // int[]row = new int[m]; --> matrix[..][0]
+        // int[]col = new int[n]; --> matrix[0][..]
+        int col1=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if (j!=0){
+                         matrix[0][j]=0;
+                    }
+                    else{
+                        col1=0;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if (matrix[0][0]==0){
+            for(int j=0;j<n;j++) matrix[0][j]=0;
+        }
+        if(col1==0){
+            for(int i=0;i<m;i++) matrix[i][0]=0;
+        }
+
+
+        System.out.println(matrix);
+}
+}
