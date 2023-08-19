b3bbacf65eec6ec2b205271745b2b88bc2db6249
Venkatesh Kocherla <86240365+Venkateshkocherla@users.noreply.github.com>
2023-08-19 22:18:28 +0530

    Add files via upload

---
+++ Day 7/Majorityelement.java
@@ -0,0 +1,22 @@
+class Majorityelement{
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                hm.put(nums[i],hm.get(nums[i])+1);
+            }
+            else{
+                hm.put(nums[i],1);
+            }
+        }
+        int max = 0;
+        int maj = 0;
+        for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
+            if(entry.getValue()>max){
+                max = entry.getValue();
+                maj = entry.getKey();
+            }
+        }
+        return maj;
+    }
+}
